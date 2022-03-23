43ac50378131153056b0d1f4d2adcfbfbed9fa54
sintadwic <sintadwicahyani23@gmail.com>
2022-03-23 22:13:58 +0700

    Praktikum

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:belanja/pages/home_page.dart';
+import 'package:belanja/pages/item_page.dart';
+
+void main() {
+  runApp(MaterialApp(initialRoute: '/', routes: {
+    '/': (context) => HomePage(),
+    '/item': (context) => const ItemPage(),
+  }
+  )
+  );
+}
